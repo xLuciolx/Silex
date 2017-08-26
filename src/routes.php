0eea9f17108f08374c517bfedfd80193b5833abe
Loïc Gallay <lgallay@orange.fr>
2017-08-26 08:29:29 +0200

    spell check (again)

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,6 @@
 use TechNews\Controller\Provider\NewsControllerProvider;
 use TechNews\Controller\Provider\AdminControllerProvider;
 
-/*gestion de nos controller via ControllerProvider*/
+/*gestion de nos controllers via ControllerProvider*/
 $app->mount('/', new NewsControllerProvider());
 $app->mount('/admin', new AdminControllerProvider());
